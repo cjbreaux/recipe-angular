adff21e38779f9145905c46a2b944cee03225073
Chris Breaux and Dino Nguyen <dino@>
2019-03-26 11:03:32 -0700

    Create recipe class and showRandom function, intialize files, add basic UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title: string = 'Recipe of the Day';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+  recipeList: Recipe[] = [
+    new Recipe('Chicken Noodle Soup', ['chicken', 'noodle', 'soup'], ['lorem', 'ipsum']),
+    new Recipe('Spaghetti', ['noodle', 'sauce'], ['lorem', 'ipsum']),
+    new Recipe('Hamburger', ['buns', 'patty'], ['lorem', 'ipsum'])
+  ];
+  randomRecipe: Recipe = showRandom(this.recipeList);
+
+  function showRandom(item) {
+    const len = item.length;
+    let randomIndex = Math.floor(Math.random() * len);
+    return item[randomIndex].title;
+  };
+
 }
+
+export class Recipe {
+  constructor(public title: string, public ingredients: string[], public directions: string[]) {}
+}
+
+// let app = new AppComponent
+// console.log(app.todaysRecipeTitle)
